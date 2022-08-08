456d348b24290c8b0bece8566927d6879e76cebb
mazharul95 <mipiyas07@gmail.com>
2022-08-08 06:12:25 +0600

    Pass Header Token & Render DataTable

--- resources/views/productAjax.blade.php
+++ resources/views/productAjax.blade.php
@@ -68,3 +68,47 @@
     </div>
 
 </body>
+
+<script type="text/javascript">
+    $(function() {
+
+                /*------------------------------------------
+                 --------------------------------------------
+                 Pass Header Token
+                 --------------------------------------------
+                 --------------------------------------------*/
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+
+                /*------------------------------------------
+                --------------------------------------------
+                Render DataTable
+                --------------------------------------------
+                --------------------------------------------*/
+                var table = $('.data-table').DataTable({
+                    processing: true,
+                    serverSide: true,
+                    ajax: "{{ route('products-ajax-crud.index') }}",
+                    columns: [{
+                            data: 'DT_RowIndex',
+                            name: 'DT_RowIndex'
+                        },
+                        {
+                            data: 'name',
+                            name: 'name'
+                        },
+                        {
+                            data: 'detail',
+                            name: 'detail'
+                        },
+                        {
+                            data: 'action',
+                            name: 'action',
+                            orderable: false,
+                            searchable: false
+                        },
+                    ]
+                });
